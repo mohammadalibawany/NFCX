c84e3df2f7e418fe66199913691951bf16d49365
mohammadalibawany <greenranger88@gmail.com>
2013-10-28 23:09:51 -0500

    Update apps.js
    Check in and checkout works in Titanium cloud

--- apps.js
+++ apps.js
@@ -3,15 +3,19 @@
 ///3 is checkout
 /// color set for debugging purposes
 
+var Cloud = require('ti.cloud');
+Cloud.debug = true;
+
 
 var win1 = Ti.UI.createWindow({
-	backgroundColor: 'blue'
+	backgroundColor: 'white'
 });
 
 var win2 = Ti.UI.createWindow({
 	backgroundColor: 'yellow'
 	
 });
+
 
 var win3 = Ti.UI.createWindow({
 	backgroundColor: 'red'
@@ -71,30 +75,55 @@ var button1 = Ti.UI.createButton({
 	height: 60
 });
 
+var button4 = Ti.UI.createButton({
+	title: 'Create and Login',
+	top: 450,
+	width: 150,
+	height: 60
+});
+
+
+
 var rem = Ti.UI.createSwitch({
 	style:Ti.UI.Android.SWITCH_STYLE_CHECKBOX,
     title:"Remember Me",
     value:false,
     top: 450
 });
-
+///_______________________****______________________________________________
 
 button1.addEventListener('click',function(e){
-	win2.open();
-	alert(usname.getValue());
+Cloud.Users.login({
+    login: 'user',
+    password: 'password'
+}, function (e) {
+    if (e.success) {
+        var user = e.users[0];
+        alert('Success:\n' +
+            'id: ' + user.id + '\n' +
+            'sessionId: ' + Cloud.sessionId + '\n' +
+            'first name: ' + user.first_name + '\n' +
+            'last name: ' + user.last_name);
+    } else {
+        alert('Error:\n' +
+            ((e.error && e.message) || JSON.stringify(e)));
+    }
+});
 	
 });
+///_____________________________*****___________
+
 
 var button2 = Ti.UI.createButton({
-	title: 'checkIN',
-	top: 200,
+	title: 'checkin',
+	top: 100,
 	width: 150,
 	height :60
 });
 
 var button3 = Ti.UI.createButton({
 	title: 'checkout',
-	top: 300,
+	top: 250,
 	width: 150,
 	height :60
 });
@@ -108,32 +137,93 @@ var CaseID = Ti.UI.createTextField({
 });
 
 
-button2.addEventListener('click',function(e){
+button4.addEventListener('click', function () {
+          Cloud.Users.create({
+            username: "user",
+            password: "password",
+            password_confirmation: "password",
+            first_name: "test",
+            last_name: "user"
+        }, function (e) {
+            if (e.success) {
+                var user = e.users[0];
+                alert('Created and logged in' + user.id);
 
-	Titanium.Geolocation.getCurrentPosition;
+                
+            }
+            else {
+                    if (e.error && e.message) {
+                        alert('Error :' + e.message);   
+                    }
+            }
+            });
+});
+
+
+///works
+button2.addEventListener('click',function(e){
+Cloud.Objects.create({
+    classname: 'Checkin',
+    fields: {
+        ID: 'user1',
+        CaseID: 'case1',
+        time: ''
+    }
+}, function (e) {
+    if (e.success) {
+        alert("Checked In Sucessfully");
+    } else {
+        alert('Error:\n' +
+            ((e.error && e.message) || JSON.stringify(e)));
+    }
+});
 });
 
 button3.addEventListener('click',function(e){
-	Titanium.Geolocation.getCurrentPosition;
-	
+Cloud.Objects.create({
+    classname: 'Checkout',
+    fields: {
+        ID: 'user1',
+        CaseID: "case1",
+        time: ''
+    }
+}, function (e) {
+    if (e.success) {
+			alert('Checkout sucessfully You just made a difference in a Kids Life');
+    } else {
+        alert('Error:\n' +
+            ((e.error && e.message) || JSON.stringify(e)));
+    }
 });
+});
+
+
+  
+/// end works
+
+
+
 
 
 view1.add(button1);
 
-view1.add(usname);
+view1.add(button2);
 
-view1.add(pwd);
+///view1.add(usname);
 
-view2.add(button2);
+///view1.add(pwd);
 
-view1.add(label1);
+//view2.add(button2);
 
-view1.add(label2);
+//view1.add(label1);
 
-view2.add(button3);
+//view1.add(label2);
 
-view1.add(rem);
+view1.add(button3);
+
+view1.add(button4);
+
+//view1.add(rem);
 
 view2.add(CaseID);
 
@@ -143,3 +233,5 @@ win2.add(view2);
 
 
 win1.open();
+
+
